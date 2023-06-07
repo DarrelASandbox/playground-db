1e4e454839e8760cfc6899bf4bafd867dc074357
DarrelA <darrelaiscoding@gmail.com>
2023-06-08 00:26:22 +0800

    `likeItem` & `unlikeItem`

--- nosql/redis/stephen-grider/02-rbay/src/services/queries/likes.ts
+++ nosql/redis/stephen-grider/02-rbay/src/services/queries/likes.ts
@@ -1,9 +1,22 @@
-export const userLikesItem = async (itemId: string, userId: string) => {};
+import { itemsKey, userLikesKey } from '$services/keys';
+import { client } from '$services/redis';
+
+export const userLikesItem = async (itemId: string, userId: string) =>
+	client.sIsMember(userLikesKey(userId), itemId);
 
 export const likedItems = async (userId: string) => {};
 
-export const likeItem = async (itemId: string, userId: string) => {};
+export const likeItem = async (itemId: string, userId: string) => {
+	const inserted = await client.sAdd(userLikesKey(userId), itemId);
 
-export const unlikeItem = async (itemId: string, userId: string) => {};
+	// Handle user pressing the like button in quick succession (2 requests in a row)
+	// We want to increment the like by only 1
+	if (inserted) return client.hIncrBy(itemsKey(itemId), 'likes', 1);
+};
+
+export const unlikeItem = async (itemId: string, userId: string) => {
+	const removed = await client.sRem(userLikesKey(userId), itemId);
+	if (removed) return client.hIncrBy(itemsKey(itemId), 'likes', -1);
+};
 
 export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
