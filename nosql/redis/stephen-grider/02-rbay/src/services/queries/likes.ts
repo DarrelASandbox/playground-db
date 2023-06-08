a2022c50de8bcd88368a62cf21b49c73cb4d4e47
DarrelA <darrelaiscoding@gmail.com>
2023-06-09 02:25:52 +0800

    Showing liked items using `SMEMBERS` & `SINTER`

--- nosql/redis/stephen-grider/02-rbay/src/services/queries/likes.ts
+++ nosql/redis/stephen-grider/02-rbay/src/services/queries/likes.ts
@@ -1,10 +1,14 @@
 import { itemsKey, userLikesKey } from '$services/keys';
 import { client } from '$services/redis';
+import { getItems } from './items';
 
 export const userLikesItem = async (itemId: string, userId: string) =>
 	client.sIsMember(userLikesKey(userId), itemId);
 
-export const likedItems = async (userId: string) => {};
+export const likedItems = async (userId: string) => {
+	const ids = await client.sMembers(userLikesKey(userId));
+	return getItems(ids);
+};
 
 export const likeItem = async (itemId: string, userId: string) => {
 	const inserted = await client.sAdd(userLikesKey(userId), itemId);
@@ -19,4 +23,7 @@ export const unlikeItem = async (itemId: string, userId: string) => {
 	if (removed) return client.hIncrBy(itemsKey(itemId), 'likes', -1);
 };
 
-export const commonLikedItems = async (userOneId: string, userTwoId: string) => {};
+export const commonLikedItems = async (userOneId: string, userTwoId: string) => {
+	const ids = await client.sInter([userLikesKey(userOneId), userLikesKey(userTwoId)]);
+	return getItems(ids);
+};
