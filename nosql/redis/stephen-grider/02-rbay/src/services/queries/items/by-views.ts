7615a1b86b4196b612bf0e8714b8ad18ab69b9a9
DarrelA <darrelaiscoding@gmail.com>
2023-06-22 22:16:00 +0800

    More argument for SORT

--- nosql/redis/stephen-grider/02-rbay/src/services/queries/items/by-views.ts
+++ nosql/redis/stephen-grider/02-rbay/src/services/queries/items/by-views.ts
@@ -4,7 +4,9 @@ import { client } from '$services/redis';
 export const itemsByViews = async (order: 'DESC' | 'ASC' = 'DESC', offset = 0, count = 10) => {
 	const results = await client.sort(itemsByViewsKey(), {
 		GET: ['#', `${itemsKey('*')}->name`, `${itemsKey('*')}->views`],
-		BY: 'score'
+		BY: 'nosort',
+		DIRECTION: order,
+		LIMIT: { offset, count }
 	});
 
 	console.log(results);
