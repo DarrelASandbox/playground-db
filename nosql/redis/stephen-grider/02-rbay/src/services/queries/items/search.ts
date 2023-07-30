070bd57d479a4ee1439a909a60099d8b0736e4e8
DarrelA <darrelaiscoding@gmail.com>
2023-07-30 19:13:03 +0800

    Parsing the search term

--- nosql/redis/stephen-grider/02-rbay/src/services/queries/items/search.ts
+++ nosql/redis/stephen-grider/02-rbay/src/services/queries/items/search.ts
@@ -1,1 +1,9 @@
-export const searchItems = async (term: string, size: number = 5) => {};
+export const searchItems = async (term: string, size: number = 5) => {
+	// Fuzzy Search Pre-Processing
+	const cleaned = term
+		.replaceAll(/[^a-zA-z0-9]/g, '')
+		.trim()
+		.split(' ')
+		.map((word) => (word ? `%${word}%` : ''))
+		.join(' ');
+};
