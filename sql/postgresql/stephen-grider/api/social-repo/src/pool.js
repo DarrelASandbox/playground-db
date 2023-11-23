9daab3cc2473641dc3a976c38336fa915d414be7
DarrelA <darrelaiscoding@gmail.com>
2023-11-24 04:22:25 +0800

    Query and close with security issue

--- sql/postgresql/stephen-grider/api/social-repo/src/pool.js
+++ sql/postgresql/stephen-grider/api/social-repo/src/pool.js
@@ -6,6 +6,15 @@ class Pool {
     this._pool = new pg.Pool(options);
     return this._pool.query('SELECT 1 + 1;');
   }
+
+  close() {
+    return this._pool.end();
+  }
+
+  // Security issue
+  query(sql) {
+    return this._pool.query(sql);
+  }
 }
 
 module.exports = new Pool();
