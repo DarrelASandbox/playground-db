5da21bcf476829cec85c4635d0cc5574c824f413
DarrelA <darrelaiscoding@gmail.com>
2023-11-24 08:33:03 +0800

    `insert`, `update` & `delete`

--- sql/postgresql/stephen-grider/api/social-repo/src/routes/users.js
+++ sql/postgresql/stephen-grider/api/social-repo/src/routes/users.js
@@ -14,8 +14,25 @@ router.get('/users/:id', async (req, res) => {
   return user ? res.send(user) : res.sendStatus(404);
 });
 
-router.post('/users', async (req, res) => {});
-router.put('/users/:id', async (req, res) => {});
-router.delete('/users/:id', async (req, res) => {});
+router.post('/users', async (req, res) => {
+  const { username, bio } = req.body;
+  const user = await UserRepo.insert(username, bio);
+  res.send(user);
+});
+
+// Need to setup update trigger in psql
+// to update `updatedAt`
+router.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { username, bio } = req.body;
+  const user = await UserRepo.update(id, username, bio);
+  return user ? res.send(user) : res.sendStatus(404);
+});
+
+router.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const user = await UserRepo.delete(id);
+  return user ? res.send(user) : res.sendStatus(404);
+});
 
 module.exports = router;
