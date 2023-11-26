aadfc12ed524ab8e708c4ce2160934765d1f655d
DarrelA <darrelaiscoding@gmail.com>
2023-11-26 13:34:00 +0800

    Programmatic schema creation

--- sql/postgresql/stephen-grider/api/social-repo/src/test/routes/users.test.js
+++ sql/postgresql/stephen-grider/api/social-repo/src/test/routes/users.test.js
@@ -6,17 +6,51 @@ const buildApp = require('../../app');
 const UserRepo = require('../../repos/user-repo');
 const pool = require('../../pool');
 
+const { randomBytes } = require('crypto');
+const { default: migrate } = require('node-pg-migrate');
+
 // Establish a connection to the database for testing purposes.
 // This is necessary here as the main connection is established in `index.js`.
-beforeAll(() =>
-  pool.connect({
+beforeAll(async () => {
+  const roleName = 'a' + randomBytes(4).toString('hex');
+
+  await pool.connect({
     host: 'localhost',
     port: 5432,
     database: 'social-repo-test',
     user: process.env.PGDB_USER,
     password: process.env.PGDB_PASSWORD,
-  })
-);
+  });
+
+  await pool.query(`CREATE ROLE ${roleName} WITH LOGIN PASSWORD '${roleName}'`);
+  await pool.query(`CREATE SCHEMA ${roleName} AUTHORIZATION ${roleName}`);
+
+  await pool.close();
+
+  // Run our migrations in the new schema
+  await migrate({
+    schema: roleName,
+    direction: 'up',
+    log: () => {},
+    noLock: true,
+    dir: 'migrations',
+    databaseUrl: {
+      host: 'localhost',
+      port: 5432,
+      database: 'social-repo-test',
+      user: roleName,
+      password: roleName,
+    },
+  });
+
+  await pool.connect({
+    host: 'localhost',
+    port: 5432,
+    database: 'social-repo-test',
+    user: roleName,
+    password: roleName,
+  });
+});
 
 // Close the database connection after all tests have completed.
 // This ensures that Jest can exit cleanly by resolving any pending database connections.
